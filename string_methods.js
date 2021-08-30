4b94eedb82d3ddb5f5350db95146c80f240d65db
JonasRossen <jona7906@stud.kea.dk>
2021-08-31 01:39:28 +0200

    html template for big_fat_string

---
+++ string_methods.js
@@ -0,0 +1,13 @@
+const string = "JoNas RosseN";
+console.log("e/17: " + string.substring(0,1).toLocaleUpperCase() + string.substring(1).toLocaleLowerCase());
+
+//const german = "Goethestraße";
+//german.lang = "en";
+//let l = german.lang;
+//console.log("e/18 " + langUsed);
+
+const string2 = "Jonas Rossen";
+console.log("e/3: " + string2[4]);
+
+const string3 = "Jonas Rossen           ";
+console.log("e/21: " + string3.trimEnd());
