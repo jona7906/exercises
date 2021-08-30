803dab92445ff045e109fdb065284a29ff79fd20
JonasRossen <jona7906@stud.kea.dk>
2021-08-30 03:55:48 +0200

    e/1.2

--- template.js
+++ template.js
@@ -1,1 +1,7 @@
-console.log("My name is Peter. \nI have a cat called Mandu");
+
+
+const man_name = "Peter";
+const cat_name = "Mandu";
+
+console.log(`My name is ${man_name}. \nI have a cat called ${cat_name}`);
+
